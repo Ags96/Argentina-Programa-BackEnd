72e5347d80cd5f157d616be1f588f012dbd3ae18
Ags96 <agustinseoane96@gmail.com>
2022-09-16 00:42:11 -0300

    HyS component
    Creación del componente HyS y modificación de rutas locales

---
+++ BackEnd/src/main/java/com/myportfolio/portfolio/Controller/CHys.java
@@ -0,0 +1,94 @@
+package com.myportfolio.portfolio.Controller;
+
+import com.myportfolio.portfolio.Dto.dtoHys;
+import com.myportfolio.portfolio.Entity.hys;
+import com.myportfolio.portfolio.Security.Controller.Mensaje;
+import com.myportfolio.portfolio.Service.Shys;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@CrossOrigin(origins = "https://miWeb.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/skill")
+public class CHys {
+
+    @Autowired
+    Shys shys;
+
+    @GetMapping("/lista")
+    public ResponseEntity<List<hys>> list() {
+        List<hys> list = shys.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<hys> getById(@PathVariable("id") int id) {
+        if (!shys.existsById(id)) {
+            return new ResponseEntity(new Mensaje("Id not exists"), HttpStatus.NOT_FOUND);
+        }
+        hys hYs = shys.getOne(id).get();
+        return new ResponseEntity(hYs, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!shys.existsById(id)) {
+            return new ResponseEntity(new Mensaje("not exists"), HttpStatus.NOT_FOUND);
+        }
+        shys.delete(id);
+        return new ResponseEntity(new Mensaje("Skill removed"), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoHys dtohys) {
+        if (StringUtils.isBlank(dtohys.getNombre())) {
+            return new ResponseEntity(new Mensaje("name is required"), HttpStatus.BAD_REQUEST);
+        }
+        if (shys.existsByNombre(dtohys.getNombre())) {
+            return new ResponseEntity(new Mensaje("this skill already exists"), HttpStatus.BAD_REQUEST);
+        }
+
+        hys hYs = new hys(dtohys.getNombre(), dtohys.getPorcentaje());
+        shys.save(hYs);
+
+        return new ResponseEntity(new Mensaje("Skill added"), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoHys dtohys) {
+        //Validamos si existe el ID
+        if (!shys.existsById(id)) {
+            return new ResponseEntity(new Mensaje("ID not exists"), HttpStatus.BAD_REQUEST);
+        }
+        //Compara nombre de skills
+        if (shys.existsByNombre(dtohys.getNombre()) && shys.getByNombre(dtohys.getNombre()).get()
+                .getId() != id) {
+            return new ResponseEntity(new Mensaje("this skill already exists"), HttpStatus.BAD_REQUEST);
+        }
+        //No puede estar vacio
+        if (StringUtils.isBlank(dtohys.getNombre())) {
+            return new ResponseEntity(new Mensaje("Name is required"), HttpStatus.BAD_REQUEST);
+        }
+
+        hys hYs = shys.getOne(id).get();
+        hYs.setNombre(dtohys.getNombre());
+        hYs.setPorcentaje(dtohys.getPorcentaje());
+
+        shys.save(hYs);
+        return new ResponseEntity(new Mensaje("Updated Skill"), HttpStatus.OK);
+
+    }
+}
